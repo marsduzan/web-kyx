d491419401838863b37ef661293f86e690e6bbcf
marscp <marscpwork@gmail.com>
2025-05-15 18:54:38 +0200

    Header Changes

--- Home/script.js
+++ Home/script.js
@@ -1,26 +1,52 @@
+// Variables y eventos menus
 const sneakk = document.querySelector('.sneakk');
 const menuSneak = document.querySelector('.menu-sneak');
+const accesorios = document.querySelector('.accesorios');
+const menuAcc = document.querySelector('.menu-acc');
 
-let timeoutId;
+let timeoutIdSneak;
+let timeoutIdAcc;
 
-function openMenu() {
-    clearTimeout(timeoutId);
+function openMenuSneak() {
+    clearTimeout(timeoutIdSneak);
     menuSneak.classList.add('active');
 }
 
-function closeMenu() {
-    timeoutId = setTimeout(() => {
+function closeMenuSneak() {
+    timeoutIdSneak = setTimeout(() => {
     if (!sneakk.matches(':hover') && !menuSneak.matches(':hover')) {
         menuSneak.classList.remove('active');
+    } 
+    }, 200);
+}
+
+function openMenuAcc() {
+    clearTimeout(timeoutIdAcc);
+    menuAcc.classList.add('active');
+}
+
+function closeMenuAcc() {
+    timeoutIdAcc = setTimeout(() => {
+    if (!accesorios.matches(':hover') && !menuAcc.matches(':hover')) {
+        menuAcc.classList.remove('active');
     }
     }, 200);
 }
 
-sneakk.addEventListener('mouseenter', openMenu);
-sneakk.addEventListener('mouseleave', closeMenu);
 
-menuSneak.addEventListener('mouseenter', openMenu);
-menuSneak.addEventListener('mouseleave', closeMenu);
+
+
+sneakk.addEventListener('mouseenter', openMenuSneak);
+sneakk.addEventListener('mouseleave', closeMenuSneak);
+
+accesorios.addEventListener('mouseenter', openMenuAcc);
+accesorios.addEventListener('mouseleave', closeMenuAcc);
+
+menuAcc.addEventListener('mouseenter', openMenuAcc);
+menuAcc.addEventListener('mouseleave', closeMenuAcc);
+
+menuSneak.addEventListener('mouseenter', openMenuSneak);
+menuSneak.addEventListener('mouseleave', closeMenuSneak);
 
 // Variables y events de carrusel
 const left = document.querySelector('#left');
