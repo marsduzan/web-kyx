ac7800c5a6c274cd2093ee263efe342d710c2968
marscp <marscpwork@gmail.com>
2025-05-15 17:39:23 +0200

    Menu Fixed

--- Home/script.js
+++ Home/script.js
@@ -1,3 +1,26 @@
+const sneakk = document.querySelector('.sneakk');
+const menuSneak = document.querySelector('.menu-sneak');
+
+let timeoutId;
+
+function openMenu() {
+    clearTimeout(timeoutId);
+    menuSneak.classList.add('active');
+}
+
+function closeMenu() {
+    timeoutId = setTimeout(() => {
+    if (!sneakk.matches(':hover') && !menuSneak.matches(':hover')) {
+        menuSneak.classList.remove('active');
+    }
+    }, 200);
+}
+
+sneakk.addEventListener('mouseenter', openMenu);
+sneakk.addEventListener('mouseleave', closeMenu);
+
+menuSneak.addEventListener('mouseenter', openMenu);
+menuSneak.addEventListener('mouseleave', closeMenu);
 
 // Variables y events de carrusel
 const left = document.querySelector('#left');
